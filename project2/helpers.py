78f45437a307c86fda4d3b7564dfc6fe6f5515a5
Daniel <dannyp95@gmail.com>
2020-11-14 20:36:53 +0100

    classification files

---
+++ project2/helpers.py
@@ -0,0 +1,69 @@
+import numpy as np
+
+
+#def sigmoid(x: np.ndarray) -> np.ndarray:
+#    return 1 / (1 + np.exp(-x))
+
+class sigmoid:
+    def __call__(self, x: np.ndarray) -> np.ndarray:
+        return 1 / (1 + np.exp(-x))
+
+    def derivative(self, x: np.ndarray) -> np.ndarray:
+        #e = np.exp(x)
+        #return e/((1 + e)**2)
+        return x*(1 - x) 
+
+class ReLU:
+    def __call__(self, x: np.ndarray) -> np.ndarray:
+        return np.maximum(np.zeros(x.shape), x)
+
+    def derivative(self, x: np.ndarray) -> np.ndarray:
+        return np.heaviside(x, 1)
+
+
+#def ReLU(x: np.ndarray) -> np.ndarray:
+#    return np.maximum(np.zeros(x.shape), x)
+
+def softmax(z: np.ndarray) -> np.ndarray:
+    #mx = np.amax(z)
+    #adjusted_z = z - mx
+    #sm =  np.sum(adjusted_z, axis=1, keepdims=True)
+    #t = adjusted_z - np.log(sm)
+    #print(mx)
+    #return np.exp(t)
+
+    #print(z[:,0])
+    #print(np.amax(z, axis=0, keepdims=True))
+    #mx = np.amax(z, axis=0)#
+    #exp = np.exp(z - mx)
+    exp = np.exp(z)
+    #t = z - np.log(np.sum(exp, axis=1, keepdims=True))
+    #return np.exp(t)
+    #print(np.sum(exp, axis=1, keepdims=True))
+    return exp / np.sum(exp, axis=1, keepdims=True)
+
+
+def softmax_der(a: np.ndarray) -> np.ndarray:
+    return a * (1 - a)
+
+def cost1(a: np.ndarray, t: np.ndarray) -> np.ndarray:
+    return a - t
+
+#derivative w.r.t a
+def cross_entropy(a: np.ndarray, t: np.ndarray):
+    return (a - t)/(a*(1 - a))
+
+def prediction(probabilities: np.ndarray) -> np.ndarray:
+    return np.argmax(probabilities, axis=1)
+
+
+def make_one_hot(vector: np.ndarray) -> np.ndarray:
+    n = len(vector)
+    categories = np.max(vector) + 1
+    onehot_vector = np.zeros((n, categories))
+    onehot_vector[range(n), vector] = 1
+    return onehot_vector
+
+
+def accuracy_score(Y_test, Y_pred):
+    return np.sum(Y_test == Y_pred) / len(Y_test)
---
+++ project2/test_NN.py
@@ -0,0 +1,97 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from time import time
+from NN import NeuralNetwork
+from helpers import *
+import seaborn as sns
+
+#element wise operation *
+#matrix multiplication @ or matmul
+
+# ensure the same random numbers appear every time
+np.random.seed(89)
+
+plt.rcParams['figure.figsize'] = (12, 12)
+
+def load_data():
+	# download MNIST dataset
+	digits = datasets.load_digits()
+
+	# define inputs and labels
+	inputs = digits.images
+	labels = digits.target
+
+	return inputs, labels
+
+
+def split_data(x, y, ratio):
+	# flatten the image
+	# the value -1 means dimension is inferred from the remaining dimensions: 8x8 = 64
+	if (x.shape[1] != None):
+		n = len(x)
+		x = x.reshape(len(x), -1)
+
+	# one-liner from scikit-learn library
+	X_train, X_test, Y_train, Y_test = train_test_split(
+	    x, y, train_size=ratio, test_size=(1 - ratio), random_state=20)
+
+	return X_train, X_test, Y_train, Y_test
+
+
+inputs, labels = load_data()
+X_train, X_test, Y_train, Y_test = split_data(inputs, labels, 0.8)
+Y_train_onehot = make_one_hot(Y_train)
+
+n, features = X_train.shape
+hidden_layer = 50
+categories = 10
+
+"""
+Create NN first, then run a grid search to find optimal values of eta and lmbda
+Then create second class that trains with the optimal parameters
+"""
+
+NN = NeuralNetwork(X_train, Y_train_onehot, features, hidden_layer, categories, sigmoid)
+a, lr, lmbda, train_accuracy, test_accuracy = NN.grid_search(X_test, Y_test, Y_train)
+
+fig, ax = plt.subplots(figsize = (10, 10))
+sns.heatmap(train_accuracy, annot=True, ax=ax, cmap="viridis")
+ax.set_title("Training Accuracy")
+ax.set_ylabel("$\eta$")
+ax.set_xlabel("$\lambda$")
+plt.show()
+
+fig, ax = plt.subplots(figsize = (10, 10))
+sns.heatmap(test_accuracy, annot=True, ax=ax, cmap="viridis")
+ax.set_title("Test Accuracy")
+ax.set_ylabel("$\eta$")
+ax.set_xlabel("$\lambda$")
+plt.show()
+
+#a = 95833... wiht lr = 0.005336699231206312 and lmb = 2.1544346900318865
+
+
+NN2 = NeuralNetwork(X_train, Y_train_onehot, features, hidden_layer, categories, sigmoid)
+lr = 0.005336699231206312
+lmbda = 2.1544346900318865
+NN2.train(lr, lmbda)
+
+prediction = NN2.make_prediction(X_test)
+
+print("Accuracy score on test set: ", accuracy_score(Y_test, prediction))
+
+X_t = X_test.reshape(-1, 8, 8)
+# choose some random images to display
+n = 6
+indices = np.arange(len(X_test))
+random_indices = np.random.choice(indices, size=n)
+for i in range(0, n):
+    plt.subplot(1, n, i+1)
+    plt.axis("off")
+    plt.imshow(X_t[random_indices[i]], cmap=plt.cm.gray_r, interpolation='nearest')
+    plt.title("Correct: %d, Prediction: %d" % (Y_test[random_indices[i]], prediction[random_indices[i]]))
+
+plt.suptitle("Guesses made by neural network trained on MNIST dataset", fontsize="25")
+plt.show()
